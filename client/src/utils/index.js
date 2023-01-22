ff1308258b6c503142904bf263c8ea82560a81c4
mohamedsakeel <mohamedsakeel@outlook.com>
2023-01-22 12:16:34 +0530

    Form field and handleChange function mplemented

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,4 +1,4 @@
-import { surpriseMePrompts } from '../components';
+import { surpriseMePrompts } from '../constants';
 
 export function getRandomPrompt(prompt) {
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
